dd5da4069c4ff23ddc962e61f69a4f60c638aa52
张爷 <zhangcandeemail@163.com>
2018-02-09 01:06:24 +0800

    添加了一个生成二维码的库

--- appdeliverweb/src/App.js
+++ appdeliverweb/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import QRCode from  'qrcode.react';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+          <QRCode value="https://www.baidu.com/"/>
       </div>
     );
   }
