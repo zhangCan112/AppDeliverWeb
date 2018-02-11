355533418f2f2f0a3846fb868ca56248606811ba
zhangcan <zclxy001@163.com>
2018-02-11 16:40:28 +0800

    规范文件路径

--- appdeliverweb/src/App.js
+++ appdeliverweb/src/App.js
@@ -1,22 +1,39 @@
 //@flow
 import React, { Component } from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch
+} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-import QRCode from  'qrcode.react';
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-          <QRCode value="https://www.baidu.com/"/>
-      </div>
+        <Router>
+            <div className="App">
+                {/**
+                 * 这里可以公共的样式,比如 头部, 尾部, 等.
+                 */}
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                {/*结合Switch组件可以匹配到都匹配不到的路劲,404等...*/}
+                <Switch>
+                    {/*<Route path='/' exact component={Home}/>*/}
+                    {/*<Route path='/user'  component={User}/>*/}
+                    {/*<Route path='/search'  component={Search}/>*/}
+                    {/*<Route path='/detail'  component={Detail}/>*/}
+                    {/*<Route path='/city'  component={City}/>*/}
+                    {/*<Route component={NotFound}/>*/}
+                </Switch>
+                footer
+            </div>
+        </Router>
     );
   }
 }
