e843ec343adaa591573be4c7f3ec9c307bd29c90
张爷 <zhangcandeemail@163.com>
2018-03-14 00:39:15 +0800

    临时提交

--- appdeliverweb/src/components/UploadPage/index.js
+++ appdeliverweb/src/components/UploadPage/index.js
@@ -2,12 +2,21 @@
 import React, { Component } from 'react';
 import UploadBox from './UploadBox';
 import '../../layouts/common.css';
+import {Input} from 'antd';
 
 type Props = {}
-type State = {}
+type State = {
+  jiras: string[]
+}
 
 
 export default class UploadPage extends Component<Props, State> {
+  constructor() {
+    super()
+    this.state = {
+      jiras: []
+    }
+  }
   render() {
     return (
       <div className='Page'>
@@ -16,7 +25,19 @@ export default class UploadPage extends Component<Props, State> {
           <span>发布测试应用,仅需两步</span>
         </div>
         <div className='HrLine'></div>
+      <div className='Step'>第一步：上传App包</div>
         <UploadBox/>
+      <div className='Step'>第二步：关联Jira问题</div>
+        {this.renderJiraList()}
+      </div>
+    )
+  }
+  renderJiraList(){
+
+    return (
+      <div className='JiraListBox'>
+        <span className='JiraText'><Input placeholder='测试'/></span>
+        <span className='JiraIcon'>123</span>
       </div>
     )
   }
