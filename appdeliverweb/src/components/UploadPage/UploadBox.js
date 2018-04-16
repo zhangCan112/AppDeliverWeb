aff7ad13d3b8aa032f4e2de0c416783f2e62fc28
zhangcan <zhangcan@xiaoshouyi.com>
2018-04-16 19:09:23 +0800

    最终还是选择用API postobject

--- appdeliverweb/src/components/UploadPage/UploadBox.js
+++ appdeliverweb/src/components/UploadPage/UploadBox.js
@@ -1,15 +1,28 @@
 //@flow
 import React, { Component } from 'react';
 import {Upload, Icon, message, Button} from 'antd';
+import {Base64} from 'js-base64';
+import hamcSha1 from 'crypto-js/hmac-sha1';
+import sha1 from 'crypto-js/sha1';
 import './index.css';
 const Dragger = Upload.Dragger;
 
+
+
+var policyText = {
+    "expiration": "2020-01-01T12:00:00.000Z", //设置该Policy的失效时间，超过这个失效时间之后，就没有办法通过这个policy上传文件了
+    "conditions": [
+    ["content-length-range", 0, 1048576000] // 设置上传文件的大小限制
+    ]
+};
+var policyBase64 = Base64.encode(JSON.stringify(policyText))
 
 type Props = {
   name: string,
   multiple: bool,
   action: string,
   headers: any,
+  data: any,
 }
 type State = {}
 
@@ -21,10 +34,22 @@ export default class UploadBox extends Component<Props, State> {
     multiple: false,
     action: '/fileUpload',
     headers: {'Access-Control-Allow-Origin' : "*"},
+    data: {}
   }
   render() {
+    let props = {
+      ...this.props,
+      data: {
+        'OSSAccessKeyId': 'LTAIP46PMViRaQCJ',
+        'key': 'zhangcan.app',
+        'success_action_status': '200',
+        'policy': policyBase64,
+        'Signature': Base64.encode(hamcSha1(policyBase64, 'T4eDGknsW2PBbDpQ9tcqTEks42tahs')),
+      }
+    }
+
     return (
-      <Dragger {...this.props} className='DraggerBox'>
+      <Dragger {...props} className='DraggerBox'>
           <div style={{padding: 50}}>
             <Button className='Button' type='primary'>
               <Icon type='upload'/> 上传应用
