591842d4aef76536524c76a5828e69ad3d2b36e1
zhangcan <zhangcan@xiaoshouyi.com>
2018-04-18 18:20:02 +0800

    解决fetch 上传文件的问题

--- appdeliverweb/src/components/UploadPage/UploadBox.js
+++ appdeliverweb/src/components/UploadPage/UploadBox.js
@@ -1,5 +1,5 @@
 //@flow
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {Upload, Icon, message, Button} from 'antd';
 import {Base64} from 'js-base64';
 import EncBase64 from 'crypto-js/enc-base64';
@@ -7,18 +7,17 @@ import hamcSha1 from 'crypto-js/hmac-sha1';
 import sha1 from 'crypto-js/sha1';
 import fs from 'fs';
 import JSZip from 'jszip';
+import fetch from 'isomorphic-fetch';
+import Promise from 'pinkie-promise';
 import './index.css';
-require('es6-promise').polyfill();
-require('isomorphic-fetch');
+
 const Dragger = Upload.Dragger;
 
-
-
 var policyText = {
-    "expiration": "2020-01-01T12:00:00.000Z", //设置该Policy的失效时间，超过这个失效时间之后，就没有办法通过这个policy上传文件了
-    "conditions": [
+  "expiration": "2020-01-01T12:00:00.000Z", //设置该Policy的失效时间，超过这个失效时间之后，就没有办法通过这个policy上传文件了
+  "conditions": [
     ["content-length-range", 0, 1048576000] // 设置上传文件的大小限制
-    ]
+  ]
 };
 var policyBase64 = Base64.encode(JSON.stringify(policyText))
 
@@ -31,16 +30,16 @@ type Props = {
 }
 type State = {}
 
-
 export default class UploadBox extends Component<Props, State> {
 
   static defaultProps = {
     name: 'file',
     multiple: false,
     action: 'https://appdeliver.oss-cn-hangzhou.aliyuncs.com/',
-    headers: {'Access-Control-Allow-Origin' : "*",
-              'Access-Control-Allow-Methods' : "POST",
-             },
+    headers: {
+      'Access-Control-Allow-Origin': "*",
+      'Access-Control-Allow-Methods': "POST"
+    },
     data: {}
   }
   render() {
@@ -51,45 +50,74 @@ export default class UploadBox extends Component<Props, State> {
         'key': 'zhangcan.app',
         'success_action_status': '200',
         'policy': policyBase64,
-        'Signature': EncBase64.stringify(hamcSha1(policyBase64, 'T4eDGknsW2PBbDpQ9tcqTEks42tahs')),
+        'Signature': EncBase64.stringify(hamcSha1(policyBase64, 'T4eDGknsW2PBbDpQ9tcqTEks42tahs'))
       },
-      beforeUpload: this.pilistUpload,
+      beforeUpload: this.beforeUpload
     }
-    return (
-      <Dragger {...props} className='DraggerBox'>
-          <div style={{padding: 50}}>
-            <Button className='Button' type='primary'>
-              <Icon type='upload'/> 上传应用
-            </Button>
-            <p className="ant-upload-text">点击或拖拽App包到上传区域</p>
-            <p className="ant-upload-hint">只支持单文件的上传，支持ipa或apk文件上传</p>
-          </div>
-      </Dragger>
-    )
+    return (<Dragger {...props} className='DraggerBox'>
+      <div style={{
+          padding: 50
+        }}>
+        <Button className='Button' type='primary'>
+          <Icon type='upload'/>
+          上传应用
+        </Button>
+        <p className="ant-upload-text">点击或拖拽App包到上传区域</p>
+        <p className="ant-upload-hint">只支持单文件的上传，支持ipa或apk文件上传</p>
+      </div>
+    </Dragger>)
   }
 
-  pilistUpload = (file: File) => {
-    let zip = new JSZip();
-  return  zip.loadAsync(file)
-    .then((zipObject) => {
-      return zipObject.files
+  beforeUpload = (file : File) => {
+    let obj = this.unzipHandler(file).then((infoPlsitFile : File) => {
+      return this.plistUpload(infoPlsitFile)
     })
-    .then((files) => {
+    let promisefunc = (resolved, rejected) => {
+      obj.then(() => {
+        rejected("我就是要报错！！！")
+      }).catch(function(error) {
+        console.log(error);
+      });
+    }
+    return new Promise(promisefunc)
+  }
+
+  unzipHandler = (file : File) => {
+    let zip = new JSZip();
+    return zip.loadAsync(file).then((zipObject) => {
+      return zipObject.files
+    }).then((files) => {
+      console.log(files);
       for (var fileName in files) {
         if (fileName.indexOf(".app/Info.plist") >= 0 && files.hasOwnProperty(fileName)) {
           return fileName;
         }
       }
-    })
-    .then((fileName) => {
-    return  zip.file(fileName).async('string')
-    })
-    .then((content) => {
-        console.log(content);
-        console.log('我是第一步');
-    })
-    .catch(function(error){
+    }).then((fileName) => {
+      return zip.file(fileName).async ('string')
+    }).then((content) => {
+      console.log('第一步结束！');
+      return new File([content], 'Info.plist')
+    }).catch(function(error) {
       console.log('error');
-     });
-   }
- }
+    });
+  }
+
+  plistUpload = (file : File) => {
+    let data = new FormData();
+    data.append('file', file);
+    let options = {
+      method: 'POST',
+      mode: 'cors',
+      redirect: 'follow',
+      body: data
+    }
+    return fetch('/fileUpload', options).then((response) => {
+      console.log('第二步结束！');
+      console.log(response.json())
+      return false
+    }).catch(function(error) {
+      console.log('error');
+    })
+  }
+}
--- appdeliverweb/src/components/UploadPage/index.js
+++ appdeliverweb/src/components/UploadPage/index.js
@@ -1,5 +1,5 @@
 //@flow
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import UploadBox from './UploadBox';
 import '../../layouts/common.css';
 import {Input, Button, Icon} from 'antd';
@@ -9,65 +9,56 @@ type State = {
   jiras: string[]
 }
 
-
 export default class UploadPage extends Component<Props, State> {
   constructor() {
     super()
     this.state = {
-      jiras: ['jira1','jira2']
+      jiras: ['jira1', 'jira2']
     }
   }
   render() {
-    return (
-      <div className='Page'>
-        <div className='Title'>
-          <span className='Left'>发布应用</span>
+    return (<div className='Page'>
+      <div className='Title'>
+        <span className='Left'>发布应用</span>
         <span>
-          <span>发布测试应用,仅需两步   </span>
-        <span>
-          <Button type="primary">发布</Button>
+          <span>发布测试应用,仅需两步
+          </span>
+          <span>
+            <Button type="primary">发布</Button>
+          </span>
         </span>
-      </span>
-        </div>
-        <div className='HrLine'></div>
+      </div>
+      <div className='HrLine'></div>
       <div className='Step'>第一步：上传App包</div>
-        <UploadBox/>
+      <UploadBox/>
       <div className='Step'>
         第二步：关联Jira问题
         <span className='JiraIcon'>
-            <Button type="dashed" shape="circle" icon="plus" />
+          <Button type="dashed" shape="circle" icon="plus"/>
         </span>
       </div>
-        {this.renderJiraList()}
-      </div>
-    )
+      {this.renderJiraList()}
+    </div>)
   }
-  renderJiraList(){
-    let jiras =this.state.jiras;
+  renderJiraList() {
+    let jiras = this.state.jiras;
     if (jiras.length == 0) {
       jiras = [null];
     }
 
-    return (
-      <div className='JiraListBox'>
-        {
-          jiras.map((obj)=>{
-            return (
-              <div className='Jira'>
-                <span className='JiraText'><Input placeholder='请输入Jira链接' defaultValue={obj}/></span>
-              {
-                (jiras.length > 1) && (
-                  <span className='JiraIcon'>
-                      <Button type="dashed" shape="circle" icon="minus" />
-                  </span>
-              )
-              }
-              </div>
-            );
-          })
-        }
-        </div>
-
-    )
+    return (<div className='JiraListBox'>
+      {
+        jiras.map((obj) => {
+          return (<div className='Jira'>
+            <span className='JiraText'><Input placeholder='请输入Jira链接' defaultValue={obj}/></span>
+            {
+              (jiras.length > 1) && (<span className='JiraIcon'>
+                <Button type="dashed" shape="circle" icon="minus"/>
+              </span>)
+            }
+          </div>);
+        })
+      }
+    </div>)
   }
 }
