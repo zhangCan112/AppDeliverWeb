99fe5f127d56b989078080e2b8d8d9fa997286fb
zhangcan <zhangcan@xiaoshouyi.com>
2018-04-24 18:46:44 +0800

    暂时提交

--- appdeliverweb/src/components/UploadPage/UploadBox.js
+++ appdeliverweb/src/components/UploadPage/UploadBox.js
@@ -13,14 +13,6 @@ import './index.css';
 
 const Dragger = Upload.Dragger;
 
-var policyText = {
-  "expiration": "2020-01-01T12:00:00.000Z", //设置该Policy的失效时间，超过这个失效时间之后，就没有办法通过这个policy上传文件了
-  "conditions": [
-    ["content-length-range", 0, 1048576000] // 设置上传文件的大小限制
-  ]
-};
-var policyBase64 = Base64.encode(JSON.stringify(policyText))
-
 type Props = {
   name: string,
   multiple: bool,
