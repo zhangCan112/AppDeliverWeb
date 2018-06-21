dbccd62a295e25fd73d0e167a2aeea73338b77fe
zhangcan <zhangcan@xiaoshouyi.com>
2018-06-21 19:13:30 +0800

    解决啃爹的顺序问题

--- appdeliverweb_ts/src/components/AppQRCodeBlock/index.tsx
+++ appdeliverweb_ts/src/components/AppQRCodeBlock/index.tsx
@@ -1,8 +1,10 @@
-import * as React from "react";
 import * as QRCode  from "qrcode.react";
+import * as React from "react";
+
+
 
 class AppQRCodeBlock extends React.Component {
-  render () {
+ public render () {
     return (
       <div>
         <image/>
